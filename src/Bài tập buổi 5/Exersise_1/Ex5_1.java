45f8cbd687d846e713b67d1dd246960a4266db6b
Germalish <112842078+Germalish@users.noreply.github.com>
2022-11-05 17:21:50 +0700

    Bài tập buổi 5-JAVA Core

---
+++ src/Bài tập buổi 5/Exersise_1/Ex5_1.java
@@ -0,0 +1,18 @@
+package Exersise_1;
+
+import java.util.Scanner;
+
+public class Ex5_1 {
+    public static void main(String[] args) {
+        Student Germalish = new Student();
+        System.out.println("Nhập id: ");
+        Germalish.id = new Scanner(System.in).nextLine();
+        System.out.println("Nhập tên: ");
+        Germalish.name = new Scanner(System.in).nextLine();
+        System.out.println("Nhập lớp: ");
+        Germalish.className = new Scanner(System.in).nextLine();
+        System.out.println("Nhập khóa: ");
+        Germalish.season = new Scanner(System.in).nextLine();
+        System.out.println("Id: " + Germalish.id + "- Họ và tên: " + Germalish.name + "- Lớp: " + Germalish.className + "- Khóa: " + Germalish.season);
+    }
+}
---
+++ src/Bài tập buổi 5/Exersise_1/Student.java
@@ -0,0 +1,14 @@
+package Exersise_1;
+
+import java.util.Scanner;
+
+public class Student {
+    public String id;
+    public String name;
+    public String className;
+    public String season;
+
+    public Student() {
+    }
+}
+
